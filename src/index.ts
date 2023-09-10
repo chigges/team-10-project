73771a16f361b7f19c130fa500841ab0fe62d5ed
CtrlAltDelight <lbchigges@gmail.com>
2023-09-10 09:10:43 -0400

    Added dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "dotenv/config"; // loads .env file into process.env. NOTE: this should be the first line
 import { setupCLI } from "./cli";
 
 console.log("before setup");
