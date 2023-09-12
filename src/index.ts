d7ba9e4eea7f6c908855a1805d33131f178cd768
CtrlAltDelight <git+git@ctrlaltdelight.org>
2023-09-12 16:02:03 -0400

    Added metric classes and basic tests for them.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
 import "dotenv/config"; // loads .env file into process.env. NOTE: this should be the first line
 import { setupCLI } from "./cli";
+import { Metric, BusFactor, Responsiveness, License, Correctness, RampUp } from "./metrics";
 
-console.log("before setup");
+const githubToken = process.env.GITHUB_TOKEN;
+const repoUrl = "https://github.com/your-org/your-repo";
+
+const metrics: Metric[] = [
+	new Responsiveness(repoUrl, githubToken),
+	new BusFactor(repoUrl, githubToken),
+	new License(repoUrl, githubToken),
+	new Correctness(repoUrl, githubToken),
+	new RampUp(repoUrl, githubToken),
+];
+
+async function evaluateAllMetrics() {
+	console.log(`{}`);
+	for (const metric of metrics) {
+		const score = await metric.evaluate();
+		console.log(`Metric: ${metric.name}`);
+		console.log(`Description: ${metric.description}`);
+		console.log(`Score: ${score}`);
+	}
+}
+
+// Run the evaluation
+
 setupCLI();
-console.log("after setup");
