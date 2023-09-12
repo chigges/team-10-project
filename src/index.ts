e4c03c896a955a55622b9bb6bf5e531c11a92b69
CtrlAltDelight <git+git@ctrlaltdelight.org>
2023-09-12 16:10:33 -0400

    Some more changes for ESLint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { Metric, BusFactor, Responsiveness, License, Correctness, RampUp } from 
 const githubToken = "my-token";
 const repoUrl = "https://github.com/your-org/your-repo";
 
+/* TODO: Uncomment this when implemented. ESLint will complain about unused variables.
 const metrics: Metric[] = [
 	new Responsiveness(repoUrl, githubToken),
 	new BusFactor(repoUrl, githubToken),
@@ -14,8 +15,10 @@ const metrics: Metric[] = [
 	new Correctness(repoUrl, githubToken),
 	new RampUp(repoUrl, githubToken),
 ];
+*/
 
-/* TODO: Implement a function like this one to evaluate all metrics
+/* TODO: Implement a function like this one to evaluate all metrics. 
+ *       ESLint will complain about unused functions.
 async function evaluateAllMetrics() {
 	console.log(`{}`);
 	for (const metric of metrics) {
