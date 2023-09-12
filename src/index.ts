23bf7bf41c89a0d06ee3ac580634e3e2c0ab3515
CtrlAltDelight <git+git@ctrlaltdelight.org>
2023-09-12 16:12:22 -0400

    Commenting things to eliminate unused variables ESLint complaint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 import "dotenv/config"; // loads .env file into process.env. NOTE: this should be the first line
 import { setupCLI } from "./cli";
+/* TODO: Uncomment this when implemented. ESLint will complain about unused variables.
 import { Metric, BusFactor, Responsiveness, License, Correctness, RampUp } from "./metrics";
 
 // TODO: make it use environment variable (process.env.GITHUB_TOKEN)
 const githubToken = "my-token";
 const repoUrl = "https://github.com/your-org/your-repo";
 
-/* TODO: Uncomment this when implemented. ESLint will complain about unused variables.
 const metrics: Metric[] = [
 	new Responsiveness(repoUrl, githubToken),
 	new BusFactor(repoUrl, githubToken),
@@ -15,10 +15,7 @@ const metrics: Metric[] = [
 	new Correctness(repoUrl, githubToken),
 	new RampUp(repoUrl, githubToken),
 ];
-*/
 
-/* TODO: Implement a function like this one to evaluate all metrics. 
- *       ESLint will complain about unused functions.
 async function evaluateAllMetrics() {
 	console.log(`{}`);
 	for (const metric of metrics) {
