32b3fbe232d8016170613369db641da149b72d1f
av1roytman <aroytman@purdue.edu>
2023-09-15 17:11:12 -0400

    Added Functionality to extract github links from npm links

--- src/URLParser.ts
+++ src/URLParser.ts
@@ -15,6 +15,49 @@ class URLParser {
 			.filter((line) => line.length > 0);
 		return urls;
 	}
+	
+	async getOnlyGithubUrls(): Promise<string[]> {
+		const allUrls = this.getUrls();
+		const npmUrls = allUrls.filter((url) => url.includes("npmjs.com"));
+		let githubUrls = allUrls.filter((url) => url.includes("github.com"));
+
+		const additionalGithubUrls: string[] = [];
+
+		for (const npmUrl of npmUrls) {
+			const link = await this.getGithubRepoFromNpm(npmUrl);
+			if (link != null) {
+				additionalGithubUrls.push(link);
+			}
+		}
+
+		return githubUrls.concat(additionalGithubUrls);
+}
+
+	async getGithubRepoFromNpm(npmUrl: string): Promise<string | null> {
+		const packageName = this.extractPackageName(npmUrl);
+		let githubLink = null;
+		if (packageName != null) {
+			const endpoint = `https://registry.npmjs.org/${packageName}`;
+			const res = await fetch(endpoint);
+			let data = await res.json();
+			data = data['repository']['url'];
+			githubLink = 'https://github.com/' + this.extractGithubRepo(data);
+		}
+
+		return githubLink || null;
+	}
+	
+	extractGithubRepo(url: string): string | null {
+		const regex = /github\.com\/([^\/]+\/[^\/]+)/;
+		const match = url.match(regex);
+		return match ? match[1] : null;
+	}
+
+	extractPackageName(url: string): string | null {
+		const regex = /https:\/\/www\.npmjs\.com\/package\/([^\/]+)/;
+		const match = url.match(regex);
+		return match ? match[1] : null;
+	}
 }
 
 export default URLParser;
