c8d3c2373b0b7df31929a78c7028fb58b822658f
Jungwoo Kwon <kwon175@purdue.edu>
2023-09-17 17:41:31 -0400

    correctness-metric

--- src/metrics.ts
+++ src/metrics.ts
@@ -191,32 +191,4 @@ export class Correctness extends BaseMetric {
 		// For now, returning a dummy value.
 		return 0.5;
 	}
-
-	// private async getIssueCounts(): Promise<{ openIssues: number; closedIssues: number }> {
-	// 	const openIssuesCount = await this.octokit
-	// 		.request("GET /repos/{owner}/{repo}/issues", {
-	// 			owner: this.owner,
-	// 			repo: this.repo,
-	// 			state: "open",
-	// 			per_page: 1,
-	// 		})
-	// 		.then(
-	// 			(response) =>
-	// 				(response as unknown as { data: { total_count: number } }).data.total_count,
-	// 		);
-
-	// 	const closedIssuesCount = await this.octokit
-	// 		.request("GET /repos/{owner}/{repo}/issues", {
-	// 			owner: this.owner,
-	// 			repo: this.repo,
-	// 			state: "closed",
-	// 			per_page: 1,
-	// 		})
-	// 		.then(
-	// 			(response) =>
-	// 				(response as unknown as { data: { total_count: number } }).data.total_count,
-	// 		);
-
-	// 	return { openIssues: openIssuesCount, closedIssues: closedIssuesCount };
-	// }
 }
