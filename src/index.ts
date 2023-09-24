796033a6f9fb77110b0abd5abdd8063d0a2c025a
FaaizMemonPurdue <fmemon@purdue.edu>
2023-09-24 18:12:01 -0400

    removed potential TODO

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
 #!/usr/bin/env node
 import "dotenv/config"; // loads .env file into process.env. NOTE: this should be the first line
 import { setupCLI } from "./cli";
-/* TODO: Uncomment this when implemented. ESLint will complain about unused variables.
-import { Metric, BusFactor, Responsiveness, License, Correctness, RampUp } from "./metrics";
-
-// TODO: make it use environment variable (process.env.GITHUB_TOKEN)
-const githubToken = "my-token";
-const repoUrl = "https://github.com/your-org/your-repo";
-
-const metrics: Metric[] = [
-	new Responsiveness(repoUrl, githubToken),
-	new BusFactor(repoUrl, githubToken),
-	new License(repoUrl, githubToken),
-	new Correctness(repoUrl, githubToken),
-	new RampUp(repoUrl, githubToken),
-];
-
-async function evaluateAllMetrics() {
-	console.log(`{}`);
-	for (const metric of metrics) {
-		const score = await metric.evaluate();
-		console.log(`Metric: ${metric.name}`);
-		console.log(`Description: ${metric.description}`);
-		console.log(`Score: ${score}`);
-	}
-}
-*/
 
 // Run the evaluation
 
