63f509d35304a8c58b7623561d3818dd205975b6
Madi Arnold <arnol179@purdue.edu>
2023-11-18 18:16:39 -0500

    First version of the /packages endpoint

--- src/controllers/packagesController.ts
+++ src/controllers/packagesController.ts
@@ -1,11 +1,26 @@
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 import { Request, Response } from 'express';
-import { PackageQuery, EnumerateOffset, PackageMetadata } from '../types'; // Adjust the path as needed
+import { PackageQuery, EnumerateOffset, PackageMetadata, AuthenticationToken } from '../types'; // Adjust the path as needed
+import { Credentials } from 'aws-sdk';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+
+const dynamoDb = new DynamoDBClient({ region: "us-east-1" });
 
 // Controller function for handling the POST request to /packages
-export const getPackages = (req: Request, res: Response) => {
+export const getPackages = async (req: Request, res: Response) => {
   try {
-    // Parse the request body
-    const requestBody: PackageQuery[] = req.body;
+
+    // Validate request body
+    const { Name, Version }: PackageQuery = req.body;
+    if (!Name) {
+      return res.status(400).json({ error: 'PackageQuery must have a Name field.' });
+    }
+
+    // Validate authentication token
+    const authToken: AuthenticationToken = req.headers['X-Authorization'] as AuthenticationToken;
+    if (!authToken || !isValidAuthToken(authToken)) {
+      return res.status(400).json({ error: 'Invalid or missing authentication token.' });
+    }
 
     // Parse query parameters safely
     let offset: EnumerateOffset | undefined;
@@ -13,9 +28,31 @@ export const getPackages = (req: Request, res: Response) => {
       offset = req.query.offset as EnumerateOffset;
     }
 
+    // Process the query and retrieve the appropriate package data from DynamoDB
+    const params = {
+      TableName: 'packages', // Replace with your DynamoDB table name
+      // Add more conditions based on your query logic
+      KeyConditionExpression: 'Name = :name' + (Version ? ' AND Version = :version' : ''),
+      ExpressionAttributeValues: marshall({
+        ':name': Name,
+        ...(Version ? { ':version': Version} : {}),
+      }),
+    };
+
+    const result = await dynamoDb.send(new QueryCommand(params));
+    // Convert DynamoDB items to PackageMetadata
+    const filteredPackages: PackageMetadata[] = result.Items ? result.Items.map((item) => {
+      const unmarshalledItem = unmarshall(item);
+      return {
+        ID: unmarshalledItem.id,
+        Name: unmarshalledItem.name,
+        Version: unmarshalledItem.version,
+      };
+    }) : [];
+
     // Process the query and retrieve the appropriate package data
     // Replace the following with your logic to fetch and filter packages based on the query
-    const filteredPackages: PackageMetadata[] = [
+    /*const filteredPackages: PackageMetadata[] = [
         {
             "Version": "1.2.3",
             "Name": "Underscore",
@@ -31,11 +68,18 @@ export const getPackages = (req: Request, res: Response) => {
             "Name": "React",
             "ID": "react"
           }
-    ]; // Your logic here
-
+    ]; // Your logic here*/
+    
     res.status(200).json(filteredPackages); // Send a JSON response with the filtered packages
   } catch (error) {
     console.error('Error handling /packages:', error);
-    res.status(500).json({ error: 'Internal Server Error' }); // Handle any errors
+    res.status(413).json({ error: 'Internal Server Error' }); // Handle any errors
   }
 };
+
+// Example function to validate the authentication token (replace with your logic)
+function isValidAuthToken(token: AuthenticationToken): boolean {
+  // Add your authentication token validation logic here
+  // For simplicity, this example assumes any non-empty token is valid
+  return !!token;
+}
