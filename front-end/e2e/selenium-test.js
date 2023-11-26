9e95c70989af5d0f1cb71207e508c2408d076acc
Caroline Gilbert <carolinecgilbert6@gmail.com>
2023-11-26 16:13:23 -0500

    selenium temp

---
+++ front-end/e2e/selenium-test.js
@@ -0,0 +1,97 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var selenium_webdriver_1 = require("selenium-webdriver");
+var fs = require('fs');
+// Specify the path to the log file
+var logFilePath = 'selenium-test.log';
+// Function to log messages to a file
+function logToFile(message) {
+    fs.appendFileSync(logFilePath, "".concat(new Date().toISOString(), " - ").concat(message, "\n"));
+}
+function reset_button() {
+    return __awaiter(this, void 0, void 0, function () {
+        var driver, resetButton, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, new selenium_webdriver_1.Builder().forBrowser('chrome').build()];
+                case 1:
+                    driver = _a.sent();
+                    _a.label = 2;
+                case 2:
+                    _a.trys.push([2, 7, 8, 10]);
+                    // Log that the test is starting
+                    logToFile('Starting Reset Button Test...');
+                    // Navigate to the app
+                    return [4 /*yield*/, driver.get('http://localhost:4200/home')];
+                case 3:
+                    // Navigate to the app
+                    _a.sent();
+                    logToFile('Navigated to http://localhost:4200/home');
+                    return [4 /*yield*/, driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.css('#button')), 10000)];
+                case 4:
+                    resetButton = _a.sent();
+                    // Click the login button
+                    return [4 /*yield*/, resetButton.click()];
+                case 5:
+                    // Click the login button
+                    _a.sent();
+                    // Wait for a moment to see the result
+                    return [4 /*yield*/, driver.sleep(3000)];
+                case 6:
+                    // Wait for a moment to see the result
+                    _a.sent();
+                    logToFile('Test completed successfully.');
+                    return [3 /*break*/, 10];
+                case 7:
+                    error_1 = _a.sent();
+                    // Log any errors that occur during the test
+                    logToFile("Error during Selenium test: ".concat(error_1));
+                    return [3 /*break*/, 10];
+                case 8:
+                    logToFile('Quitting Selenium test...');
+                    return [4 /*yield*/, driver.quit()];
+                case 9:
+                    _a.sent();
+                    return [7 /*endfinally*/];
+                case 10: return [2 /*return*/];
+            }
+        });
+    });
+}
+reset_button();
